5c38e0f85908ab30f55828d61a9a21f46b7a35e7
Pedro Fernandes <pedroice@gmail.com>
2026-09-02 12:03:45 +0100

    test(status): validate native directive package

--- projects/components/type-tests/status.type-test.ts
+++ projects/components/type-tests/status.type-test.ts
@@ -1,4 +1,9 @@
 import { type ZdStatusColor, type ZdStatusSize, ZdStatus } from '@pranxy/zordon-ui/status';
-const color: ZdStatusColor = 'success'; const size: ZdStatusSize = 'xl'; void color; void size; void ZdStatus;
+const color: ZdStatusColor = 'success';
+const size: ZdStatusSize = 'xl';
+void color;
+void size;
+void ZdStatus;
 // @ts-expect-error Status colors are upstream candidates.
-const invalidColor: ZdStatusColor = 'brand'; void invalidColor;
+const invalidColor: ZdStatusColor = 'brand';
+void invalidColor;
